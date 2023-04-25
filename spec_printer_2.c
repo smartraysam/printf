951fd8e98c0477a747712b68d4cabf06d1a3cc48
Smart  Raysam <engsamtob@yahoo.com>
2023-04-26 00:12:03 +0100

    task 10 resolved

--- spec_printer_2.c
+++ spec_printer_2.c
@@ -91,38 +91,6 @@ void convert_fmt_S(va_list *args_list, fmt_info_t *fmt_info)
 }
 
 /**
- * convert_fmt_r - Prints the reversed string
- * @args_list: The arguments list
- * @fmt_info: The format info
- *
- * Return: The number of characters written
- */
-void convert_fmt_r(va_list *args_list, fmt_info_t *fmt_info)
-{
-	int i, len;
-	char *str = va_arg(*args_list, char *);
-
-	if (str)
-	{
-		len = str_len(str);
-		if (!fmt_info->left)
-		{
-			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-				_putchar(fmt_info->pad);
-		}
-		for (i = len - 1; i >= 0; i--)
-		{
-			_putchar(*(str + i));
-		}
-		if (fmt_info->left)
-		{
-			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-				_putchar(fmt_info->pad);
-		}
-	}
-}
-
-/**
  * convert_fmt_R - Prints the rot13'ed string
  * @args_list: The arguments list
  * @fmt_info: The format info
@@ -154,6 +122,38 @@ void convert_fmt_R(va_list *args_list, fmt_info_t *fmt_info)
 					+ ((1 - is_low_1 - is_low_2) * (*(str + i) - 13)));
 			else
 				_putchar(*(str + i));
+		}
+		if (fmt_info->left)
+		{
+			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+				_putchar(fmt_info->pad);
+		}
+	}
+}
+
+/**
+ * convert_fmt_r - Prints the reversed string
+ * @args_list: The arguments list
+ * @fmt_info: The format info
+ *
+ * Return: The number of characters written
+ */
+void convert_fmt_r(va_list *args_list, fmt_info_t *fmt_info)
+{
+	int i, len;
+	char *str = va_arg(*args_list, char *);
+
+	if (str)
+	{
+		len = str_len(str);
+		if (!fmt_info->left)
+		{
+			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
+				_putchar(fmt_info->pad);
+		}
+		for (i = len - 1; i >= 0; i--)
+		{
+			_putchar(*(str + i));
 		}
 		if (fmt_info->left)
 		{
