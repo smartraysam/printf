5e5333e1671bc40a738876713a0ae0d5f533b2d6
smartraysam <raysamtob@gmail.com>
2023-04-25 22:12:20 +0100

    add S spec print

--- spec_printer_2.c
+++ spec_printer_2.c
@@ -74,9 +74,7 @@ void convert_fmt_S(va_list *args_list, fmt_info_t *fmt_info)
 				_putchar(TO_UPPER(hex_digit(*(str + i) % 16)));
 			}
 			else
-			{
 				_putchar(*(str + i));
-			}
 		}
 		if (fmt_info->left)
 		{
@@ -85,9 +83,7 @@ void convert_fmt_S(va_list *args_list, fmt_info_t *fmt_info)
 		}
 	}
 	else
-	{
 		_putstr("(null)");
-	}
 }
 
 /**
@@ -122,38 +118,6 @@ void convert_fmt_R(va_list *args_list, fmt_info_t *fmt_info)
 					+ ((1 - is_low_1 - is_low_2) * (*(str + i) - 13)));
 			else
 				_putchar(*(str + i));
-		}
-		if (fmt_info->left)
-		{
-			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-				_putchar(fmt_info->pad);
-		}
-	}
-}
-
-/**
- * convert_fmt_r - Prints the reversed string
- * @args_list: The arguments list
- * @fmt_info: The format info
- *
- * Return: The number of characters written
- */
-void convert_fmt_r(va_list *args_list, fmt_info_t *fmt_info)
-{
-	int i, len;
-	char *str = va_arg(*args_list, char *);
-
-	if (str)
-	{
-		len = str_len(str);
-		if (!fmt_info->left)
-		{
-			for (i = 0; i < MAX(len, fmt_info->width) - len; i++)
-				_putchar(fmt_info->pad);
-		}
-		for (i = len - 1; i >= 0; i--)
-		{
-			_putchar(*(str + i));
 		}
 		if (fmt_info->left)
 		{
