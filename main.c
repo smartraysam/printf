3e011bc67bf9c7c981b6352beeeb94815a2b69f2
Smart  Raysam <engsamtob@yahoo.com>
2023-04-25 17:43:30 +0100

    convert_fmt_di - Prints a signed integer

--- main.c
+++ main.c
@@ -20,5 +20,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
     return (0);
 }
