b62999811507981a8218f95f5464e4ae4622966a
Smart  Raysam <engsamtob@yahoo.com>
2023-04-26 00:01:26 +0100

    Task 10

--- _printf.c
+++ _printf.c
@@ -68,6 +68,8 @@ void write_format(va_list *args_list, fmt_info_t *fmt_info)
 		{'p', convert_fmt_p},
 		{'r', convert_fmt_r},
 		{'R', convert_fmt_R},
+		{'F', convert_fmt_fF},
+		{'f', convert_fmt_fF},
 	};
 
 	for (i = 0; i < 23 && spec_printers[i].spec != '\0'; i++)
